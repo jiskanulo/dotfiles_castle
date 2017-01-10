b453ca135aa409f486ccc887afef8bffdb46f95c
yusuke mori <jiska.ym+github@gmail.com>
2017-01-11 01:45:34 +0900

    Add apm package: language-babel

--- home/.atom/install-packages.sh
+++ home/.atom/install-packages.sh
@@ -3,6 +3,7 @@
 apm install atom-beautify
 apm install atom-material-syntax
 apm install atom-material-ui
+apm install language-babel
 apm install editorconfig
 apm install platformio-ide-terminal
 apm install sort-lines
