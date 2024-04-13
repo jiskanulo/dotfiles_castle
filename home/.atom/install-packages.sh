f776afd696e7b18526872bc20fcdf1471f4dffc5
yusuke mori <210861+jiskanulo@users.noreply.github.com>
2024-04-13 13:32:17 +0900

    Remove .atom

--- home/.atom/install-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apm install atom-beautify
-apm install atom-material-syntax
-apm install atom-material-ui
-apm install language-babel
-apm install editorconfig
-apm install platformio-ide-terminal
-apm install sort-lines
