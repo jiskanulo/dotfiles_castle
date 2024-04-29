3bca2ca5c28a80db8d6d09fa4893827272f81248
yusuke mori <210861+jiskanulo@users.noreply.github.com>
2024-04-30 00:46:16 +0900

    Adjust hammerspoon to use BundleIdentifer for app switching

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,10 +1,21 @@
-hs.hotkey.bind({ "cmd", "shift" }, "6", function()
-  local appName = "alacritty"
-  local app = hs.application.find(appName)
+--- Launch or toggle App.
+-- @see   https://www.hammerspoon.org/docs/hs.hotkey.html#bind
+-- @param mods     A table or a string containing (as elements, or as substrings with any separator) the keyboard modifiers required.
+-- @param key      A string containing the name of a keyboard key (as found in hs.keycodes.map ), or a raw keycode number.
+-- @param bundleID Application Bundle Identifier
+function toggleApp(mods, key, bundleID)
+  hs.hotkey.bind(mods, key, function()
+    local app = hs.application.get(bundleID)
+    if app == nil then
+      hs.application.launchOrFocusByBundleID(bundleID)
+    elseif app:isFrontmost() then
+      app:hide()
+    else
+      hs.application.launchOrFocusByBundleID(bundleID)
+    end
+  end)
+end
 
-  if app == nil or app:isHidden() then
-    hs.application.launchOrFocus(appName)
-  else
-    app:hide()
-  end
-end)
+-- ❯ lsappinfo info -only bundleid 'Alacritty'
+-- "CFBundleIdentifier"="org.alacritty"
+toggleApp({"cmd", "shift"}, "6", "org.alacritty")
